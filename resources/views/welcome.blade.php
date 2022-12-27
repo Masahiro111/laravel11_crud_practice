024b9215569055d72ced884536c5d0e68a89a6f4
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-12-27 19:39:12 +0900

    laravel component practive 2/5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
-
 @section('title','Laravel Components')
+
+@section('sidebar')
+@parent
+<p>サイドバーに追加できます。</p>
+@endsection
 
 @section('content')
 <h1 class="text-2xl">Laravel Components</h1>
